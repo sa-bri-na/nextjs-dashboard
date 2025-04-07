7d6aacfa24566c600debd6c574e827024792ae92
sa-bri-na <huber_sabrina@me.com>
2025-04-07 14:49:06 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// good practice to add the global.cc in the top level layout
+import "@/app/ui/global.css"
+import {inter} from "@/app/ui/fonts"
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
